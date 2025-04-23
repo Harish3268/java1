7c75daf32e9d279607e82aef967ff4193147cf60
Harish3268 <harishpadatare100@gmail.com>
2025-04-23 16:55:45 +0530

    first

--- demo.java
+++ demo.java
@@ -1,16 +1,17 @@
-class demo
-{
+class demo {
     public static void main(String[] args) {
-        int a=10;
-        int b=5;
-        int sum = a+b;
-        int sub = a-b;
-        int mult = a*b;
-        int div = a/b;
-        System.out.println("sum is ="+sum);
-        System.out.println("sub is ="+sub);
-        System.out.println("mult is ="+mult);
-        System.out.println("div is ="+div);
-    }
+        int a = 10;
+        int b = 5;
 
+        int sum = a + b;
+        int sub = a - b;
+        int mult = a * b;
+
+        String div = (b != 0) ? String.valueOf(a / b) : "Undefined (cannot divide by zero)";
+
+        System.out.println("Sum is = " + sum);
+        System.out.println("Sub is = " + sub);
+        System.out.println("Mult is = " + mult);
+        System.out.println("Div is = " + div);
     }
+}
