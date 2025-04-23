5293373b40f1f33fbb32937e027006855fedd969
Harish3268 <harishpadatare100@gmail.com>
2025-04-23 15:00:14 +0530

    first

--- demo.java
+++ demo.java
@@ -5,7 +5,12 @@ class demo
         int b=5;
         int sum = a+b;
         int sub = a-b;
+        int mult = a*b;
+        int div = a/b;
         System.out.println("sum is ="+sum);
         System.out.println("sub is ="+sub);
+        System.out.println("mult is ="+mult);
+        System.out.println("div is ="+div);
     }
-}
+
+    }
